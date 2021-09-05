d64d76d0cf3548005b703fa4639aebbc05991f42
patern0ster <karasakal99@gmail.com>
2021-09-05 17:42:00 +0300

    changed from puppeteer to axios&cheerio

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM node:14
 WORKDIR /app
-COPY package.json /app
+COPY package.json .
 RUN npm install
-COPY . /app
+COPY . .
 EXPOSE 3000
 VOLUME [ "/app/node_modules" ]
 CMD ["npm", "start"]
--- utils/scraper.js
+++ utils/scraper.js
@@ -1,29 +1,19 @@
-const puppeteer = require("puppeteer");
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 module.exports = async (url) => {
-
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
-
-    const [el] = await page.$x(`//*[@id="listing-right-column"]/div/div[1]/div[1]/div/div/div/div/div[1]/ul/li[1]/img`);
-    const src = await el.getProperty('src');
-    const srcTxt = await src.jsonValue();
-
-    const [el2] = await page.$x(`//*[@id="listing-page-cart"]/div[3]/div[1]/div[1]/div/div[1]/p[1]`);
-    const price = await el2.getProperty('textContent');
-    const priceTxt = JSON.stringify(await price.jsonValue()).replace(/,/g, '').match(/\d+.\d+/)[0];
-
-    const [el3] = await page.$x(`//*[@id="listing-page-cart"]/div[2]/h1`);
-    const name = await el3.getProperty('textContent');
-    const nameTxt = JSON.stringify(await name.jsonValue()).replace(/\\n|"/g,'').trim();
-
-    browser.close();
-    return {
-        name: nameTxt,
-        image: srcTxt,
-        price: priceTxt
-    }
+    axios.get(url)
+    .then(response => {
+        const $ = cheerio.load(response.data);
+        const name = $('h1.wt-text-body-03').html().trim();
+        const image = $('li.wt-position-absolute > img').attr('src');
+        const price = $('div.wt-mb-lg-0.wt-mb-xs-6[data-buy-box=""]>div>div>div>div>p').html().trim().replace(/,/g, '').match(/\d+.\d+/)[0];
+        console.log( {
+            name,
+            image,
+            price
+        });
+    });
 }
 
 
